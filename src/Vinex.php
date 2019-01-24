9554b7be28c97d41e8b85694d24e8a9ae1bbf28d
pxgamer <owzie123@gmail.com>
2019-01-24 22:41:07 +0000

    Add Vinex base class

---
+++ src/Vinex.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace pxgamer\Vinex;
+
+class Vinex
+{
+    /** The base URI for the VINEX main site. */
+    public const BASE_URI = 'https://vinex.network';
+    /** The base URI for the VINEX API. */
+    public const API_BASE_URI = 'https://api.vinex.network/';
+}
