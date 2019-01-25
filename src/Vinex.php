dbf2adaff525092e48099b6d89effda094f1865d
pxgamer <owzie123@gmail.com>
2019-01-25 11:51:06 +0000

    Add HttpAdapter class for communication

--- src/Vinex.php
+++ src/Vinex.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace pxgamer\Vinex;
 
+use pxgamer\Vinex\Adapter\HttpAdapter;
+
 class Vinex
 {
-    /** The base URI for the VINEX main site. */
-    public const BASE_URI = 'https://vinex.network';
-    /** The base URI for the VINEX API. */
-    public const API_BASE_URI = 'https://api.vinex.network/';
+    /** @var HttpAdapter */
+    protected $adapter;
+
+    public function __construct(HttpAdapter $adapter)
+    {
+        $this->adapter = $adapter;
+    }
 }
